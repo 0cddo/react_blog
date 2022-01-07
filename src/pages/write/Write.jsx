9b0a07a381775c40dfd17bfa16853c9344d06ebc
0cddo <0.c.dddo@gmail.com>
2022-01-07 22:13:27 +0900

    ✨ Create write page

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,36 @@
+import "./write.css";
+
+export default function Write() {
+  return (
+    <div className="write">
+      {/* file 클릭시 예상 이미지 */}
+      <img
+        className="writeImg"
+        src={require("../../components/sidebar/aboutme.jpg")}
+        alt="write sample"
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+}
