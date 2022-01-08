0fdf1cd93496d501a30a48265ba41a7acfc821f3
0cddo <0.c.dddo@gmail.com>
2022-01-08 21:38:54 +0900

    ⚡️ 각 컴포넌트 라우트하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <div>
+    <BrowserRouter>
       <TopBar />
-      <Home />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
